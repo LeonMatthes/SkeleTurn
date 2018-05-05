38f671927c17820c5c5c1a4c74445fe749b23915
Leon <leon@matthes.biz>
2018-05-05 11:18:41 +0200

    Added ability to flip spawns gravity and fixed collision in levels

--- scenes/Spawn.gd
+++ scenes/Spawn.gd
@@ -2,6 +2,7 @@ extends Position2D
 
 var collisionArea = null
 var Player = preload("res://scenes/Player.tscn")
+export (bool) var turnGravity = false
 
 func _ready():
 	collisionArea = $SpawnArea
@@ -12,7 +13,7 @@ func canSpawnPlayer():
 func spawnPlayer(var gameController, var playerNumber):
 	if !self.canSpawnPlayer():
 		breakpoint
-	
+
 	var player = Player.instance()
 	gameController.add_child(player)
-	player.initialize(gameController, self.position, playerNumber)
+	player.initialize(gameController, self.position, playerNumber, -1 if turnGravity else 1)
